0fd086b8a97c44d3ec6614643c37741137f4817b
Phyb1 <phibeon@yahoo.com>
2026-06-07 15:14:34 +0200

    static fix

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.insert(0, '/home/mathxuco/thosheck/public_html')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'yourprojectname.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'thosheck.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
